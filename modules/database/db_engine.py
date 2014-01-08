a47b1b44608b50a64decea094bec8c1e40fe04bf
susemine <susemeee@gmail.com>
2014-01-08 13:20:37 +0900

    Added Disconnection listener

--- modules/database/db_engine.py
+++ modules/database/db_engine.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import OperationalError
+from sqlalchemy.exc import OperationalError, DisconnectionError
 
 cIntra_user_id = "closeapi"
 cIntra_user_pw = "82YwNQxLvMQeGeVx"
@@ -18,11 +18,26 @@ WHITE = '\033[0m'		#ffffff
 class Intra_Database:
 	def __init__(self):
 		try:
-			self.connect_url = "mysql://{0}:{1}@127.0.0.1/{2}?charset=utf8".format(cIntra_user_id, cIntra_user_pw, cIntra_uesr_db)
-			self.db_conn = create_engine(self.connect_url, pool_recycle=5).connect()
+			connect_url = "mysql://{0}:{1}@127.0.0.1/{2}?charset=utf8".format(cIntra_user_id, cIntra_user_pw, cIntra_uesr_db)
+			db_engine = create_engine(connect_url, pool_recycle=5)
+			event.listen(db_engine, 'checkout', self.checkout_listener)
+
+			self.db_conn = db_engine.connect()
 			#sessionmaker() returns "class"
 			self.session = (sessionmaker(bind=self.db_conn))()
+			
 		except OperationalError:
 			print RED+"DBError : please check database connection."+WHITE
 			exit(1)
 
+	def checkout_listener(self, dbapi_con, con_record, con_proxy):
+	    try:
+	        try:
+	            dbapi_con.ping(False)
+	        except TypeError:
+	            dbapi_con.ping()
+	    except dbapi_con.OperationalError as exc:
+	        if exc.args[0] in (2006, 2013, 2014, 2045, 2055):
+	            raise DisconnectionError()
+	        else:
+	            raise
--- modules/database/db_raiseable.py
+++ modules/database/db_raiseable.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 class Raiseable:
@@ -6,7 +8,13 @@ class Raiseable:
 	Debug = 99
 
 	error_map = {
+		"MultipleResultsFound" : -301,
+		"NoResultFound" : -302
+	}
 
+	error_msg_map = {
+		"MultipleResultsFound" : "의도되지 않은 중복된 값 발견",
+		"NoResultFound" : "존재하지 않는 값입니다."
 	}
 
 	def raise_error(self, etype, e_from, e=None):
@@ -18,20 +26,18 @@ class Raiseable:
 
 		#clearing result for avoiding confilcts
 		self._res = {'event' : {}}
-
-		self._res['event'].update({"error_type" : etype, "error_from" : e_from})
+		self._res['Title'] = "의도적 내부 오류"
+		self._res['event'].update({"error_type" : etype, "error_from" : e_from, "error" : str(type(e))})
 		
 		if e is not None:
+			self._res['Title'] = "내부 오류"
 			self._res['event']['error_dmp'] = str(e)
 			#assigning error_code(status)		
 			#find error code in mapper object
 			try:
 				self._res['event']['status'] = Raiseable.error_map[str(type(e))]
+				self._res['Message'] = Raiseable.error_msg_map[str(type(e))]
 			except KeyError:
-				if type(e) is MultipleResultsFound:
-					self._res['event'].update({"status" : -301, "message" : "unexpected multiple result found"})
-				elif type(e) is NoResultFound:
-					self._res['event'].update({"status" : -302, "message" : "no result found"})
-				else:
-					self._res['event'].update({"status" : -302, "message" : "Unhandled Exception"})
+				self._res['event']["status"] = -399
+				self._res['Message'] = "예상되지 않은 "+str(type(e))+"@"+e_from
 
