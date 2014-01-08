a3e8f02ac0ed9cc6bb4977f3fd526ac4f6b1ecd7
susemine <susemeee@gmail.com>
2014-01-08 14:54:52 +0900

    Polished exHandling even more

--- modules/database/db_raiseable.py
+++ modules/database/db_raiseable.py
@@ -3,8 +3,8 @@
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 class Raiseable:
-	DataError = 0
-	UserError = 1
+	DataError = 30
+	UserError = 10
 	Debug = 99
 
 	error_map = {
@@ -27,7 +27,7 @@ class Raiseable:
 		#clearing result for avoiding confilcts
 		self._res = {'event' : {}}
 		self._res['Title'] = "의도적 내부 오류"
-		self._res['event'].update({"error_type" : etype, "error_from" : e_from, "error" : str(type(e))})
+		self._res['event'].update({"error_type" : etype, "error_from" : e_from, "error" : name(e)})
 		
 		if e is not None:
 			self._res['Title'] = "내부 오류"
@@ -35,9 +35,11 @@ class Raiseable:
 			#assigning error_code(status)		
 			#find error code in mapper object
 			try:
-				self._res['event']['status'] = Raiseable.error_map[str(type(e))]
-				self._res['Message'] = Raiseable.error_msg_map[str(type(e))]
+				self._res['event']['status'] = Raiseable.error_map[name(e)]
+				self._res['Message'] = Raiseable.error_msg_map[name(e)]
 			except KeyError:
 				self._res['event']["status"] = -399
-				self._res['Message'] = "예상되지 않은 "+str(type(e))+"@"+e_from
+				self._res['Message'] = "예상되지 않은 "+name(e)+"@"+e_from
 
+def name(exc):
+	return exc.__class__.__name__
